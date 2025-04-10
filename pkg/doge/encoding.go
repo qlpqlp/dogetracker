615c1d2a4542ce2b6b7c2701ec65f3f217ebfc07
inevitable360 <pvidalx@gmail.com>
2025-04-10 22:07:52 +0100

    Fix bugs

--- pkg/doge/encoding.go
+++ pkg/doge/encoding.go
@@ -1,115 +1,102 @@
 package doge
 
 import (
+	"encoding/binary"
 	"encoding/hex"
-	"errors"
-	"log"
+	"fmt"
 )
-
-// HexDecode decodes a hex string into bytes
-func HexDecode(s string) ([]byte, error) {
-	return hex.DecodeString(s)
-}
 
 // DecodeBlock decodes a block from raw bytes
 func DecodeBlock(data []byte) (*Block, error) {
 	if len(data) < 80 {
-		return nil, errors.New("block data too short")
+		return nil, fmt.Errorf("block data too short")
 	}
 
 	block := &Block{}
 	offset := 0
 
-	// Decode header
-	// The version is stored in little-endian format
+	// Decode version
 	versionBytes := data[offset : offset+4]
-	block.Header.Version = uint32(versionBytes[0]) | uint32(versionBytes[1])<<8 | uint32(versionBytes[2])<<16 | uint32(versionBytes[3])<<24
+	block.Header.Version = int32(binary.LittleEndian.Uint32(versionBytes))
 	offset += 4
 
-	// Log block version and type
-	// The version is stored in little-endian format, so we need to check the actual value
-	// 0x20000000 in little-endian is 0x00000020
-	isAuxPow := (block.Header.Version & 0x20000000) != 0
-	log.Printf("Block version bytes: %x", versionBytes)
-	log.Printf("Block version: %x (is AuxPow: %v)", block.Header.Version, isAuxPow)
-
-	block.Header.PrevBlock = make([]byte, 32)
-	copy(block.Header.PrevBlock, data[offset:offset+32])
+	// Decode previous block hash
+	var prevBlock [32]byte
+	copy(prevBlock[:], data[offset:offset+32])
+	block.Header.PrevBlock = prevBlock
 	offset += 32
 
-	block.Header.MerkleRoot = make([]byte, 32)
-	copy(block.Header.MerkleRoot, data[offset:offset+32])
+	// Decode merkle root
+	var merkleRoot [32]byte
+	copy(merkleRoot[:], data[offset:offset+32])
+	block.Header.MerkleRoot = merkleRoot
 	offset += 32
 
-	block.Header.Timestamp = uint32(data[offset]) | uint32(data[offset+1])<<8 | uint32(data[offset+2])<<16 | uint32(data[offset+3])<<24
+	// Decode timestamp
+	block.Header.Time = binary.LittleEndian.Uint32(data[offset : offset+4])
 	offset += 4
 
-	block.Header.Bits = uint32(data[offset]) | uint32(data[offset+1])<<8 | uint32(data[offset+2])<<16 | uint32(data[offset+3])<<24
+	// Decode bits
+	block.Header.Bits = binary.LittleEndian.Uint32(data[offset : offset+4])
 	offset += 4
 
-	block.Header.Nonce = uint32(data[offset]) | uint32(data[offset+1])<<8 | uint32(data[offset+2])<<16 | uint32(data[offset+3])<<24
+	// Decode nonce
+	block.Header.Nonce = binary.LittleEndian.Uint32(data[offset : offset+4])
 	offset += 4
 
-	// Check if this is an AuxPow block
-	if isAuxPow {
-		log.Printf("Processing AuxPow block, skipping AuxPow data")
-		// For AuxPow blocks, we need to handle the special structure
-		// 1. First comes the transaction count (varint)
-		txCount, n := DecodeVarInt(data[offset:])
-		if n == 0 {
-			return nil, errors.New("invalid transaction count")
-		}
-		offset += n
-		log.Printf("AuxPow block has %d transactions", txCount)
+	// Decode transaction count
+	txCount, bytesRead := DecodeVarInt(data[offset:])
+	offset += bytesRead
 
-		if txCount > 0 {
-			// 2. The first transaction is the coinbase transaction
-			// For AuxPow blocks, we only process the coinbase transaction
-			// and skip the rest of the AuxPow data
-			tx, err := DecodeTransaction(data[offset:])
-			if err != nil {
-				log.Printf("Error decoding coinbase transaction in AuxPow block: %v", err)
-				return nil, err
-			}
-			block.Tx = append(block.Tx, *tx)
-			log.Printf("Successfully decoded coinbase transaction in AuxPow block")
-
-			// Skip the rest of the AuxPow data
-			// The AuxPow data structure is:
-			// - Coinbase transaction (already processed)
-			// - AuxPow block header (80 bytes)
-			// - AuxPow Merkle branch
-			// - AuxPow parent block header (80 bytes)
-			// We don't need to process this data for our purposes
-			return block, nil
-		}
-		return block, nil
-	}
-
-	// For regular blocks, decode all transactions
-	txCount, n := DecodeVarInt(data[offset:])
-	if n == 0 {
-		return nil, errors.New("invalid transaction count")
-	}
-	offset += n
-	log.Printf("Regular block has %d transactions", txCount)
-
+	// Decode transactions
 	block.Tx = make([]Transaction, txCount)
 	for i := uint64(0); i < txCount; i++ {
 		tx, err := DecodeTransaction(data[offset:])
 		if err != nil {
-			log.Printf("Error decoding transaction %d in regular block: %v", i, err)
-			return nil, err
+			return nil, fmt.Errorf("error decoding transaction %d: %v", i, err)
 		}
 		block.Tx[i] = *tx
-		// Move offset to next transaction
 		offset += tx.SerializeSize()
 	}
 
 	return block, nil
 }
 
-// DecodeTx is an alias for DecodeTransaction
-func DecodeTx(data []byte) (*Transaction, error) {
-	return DecodeTransaction(data)
+// DecodeVarInt decodes a variable-length integer from the input bytes
+// Returns the decoded value and the number of bytes read
+func DecodeVarInt(data []byte) (uint64, int) {
+	if len(data) == 0 {
+		return 0, 0
+	}
+
+	// Read the first byte to determine the format
+	firstByte := data[0]
+	if firstByte < 0xfd {
+		// Single byte
+		return uint64(firstByte), 1
+	} else if firstByte == 0xfd {
+		// 2 bytes
+		if len(data) < 3 {
+			return 0, 0
+		}
+		return uint64(data[1]) | uint64(data[2])<<8, 3
+	} else if firstByte == 0xfe {
+		// 4 bytes
+		if len(data) < 5 {
+			return 0, 0
+		}
+		return uint64(data[1]) | uint64(data[2])<<8 | uint64(data[3])<<16 | uint64(data[4])<<24, 5
+	} else {
+		// 8 bytes
+		if len(data) < 9 {
+			return 0, 0
+		}
+		return uint64(data[1]) | uint64(data[2])<<8 | uint64(data[3])<<16 | uint64(data[4])<<24 |
+			uint64(data[5])<<32 | uint64(data[6])<<40 | uint64(data[7])<<48 | uint64(data[8])<<56, 9
+	}
+}
+
+// HexDecode decodes a hex string to bytes
+func HexDecode(hexStr string) ([]byte, error) {
+	return hex.DecodeString(hexStr)
 }
