706b24edbfa7e7b2b934172da806ca1bbbfe9ef9
inevitable360 <pvidalx@gmail.com>
2025-04-15 00:00:47 +0100

    removing the unused blockHex

--- server/main.go
+++ server/main.go
@@ -51,16 +51,6 @@ func processBlock(ctx context.Context, db *database.DB, blockchain spec.Blockcha
 	log.Printf("Processing block: Height=%d, Hash=%s, Time=%d, Confirmations=%d",
 		header.Height, header.Hash, header.Time, header.Confirmations)
 
-	// Get raw block data
-	blockHex, err := blockchain.GetBlock(hash)
-	if err != nil {
-		return fmt.Errorf("error getting block data: %v", err)
-	}
-
-	// Parse block data (you'll need to implement this based on your blockchain library)
-	// For now, we'll just log that we're processing the block
-	log.Printf("Processing transactions in block %d", height)
-
 	// Get all tracked addresses
 	rows, err := db.Query("SELECT id, address FROM addresses")
 	if err != nil {
